33fbf910dcddaae3879d1d04fa17b6eeb005f5bd
adanhazi <adan.hazi10@gmail.com>
2025-02-17 12:23:46 +0300

    canIVote function

---
+++ JS_Functions/canIVote/main.js
@@ -0,0 +1,14 @@
+// Write your function here:
+function canIVote(number){
+    if (number >= 18){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  
+  console.log(canIVote(19)) // Should print true
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
