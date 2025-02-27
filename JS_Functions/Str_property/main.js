02dc7c162706b0f6d4e553a033351fdab630fa7e
adanhazi <adan.hazi10@gmail.com>
2025-02-27 22:02:30 +0300

    unfinished str proprties

--- JS_Functions/Str_property/main.js
+++ JS_Functions/Str_property/main.js
@@ -11,3 +11,10 @@ console.log(str[4]); // "S"
 //3.1. constructor
 let sample = "learn";
 console.log(sample.constructor === String); // true
+
+//3.2. [Symbol.iterator]
+let text = "iterate";
+for (const char of text) {
+  console.log(char);
+}
+// This logs each character: "i", "t", "e", "r", "a", "t", "e"
