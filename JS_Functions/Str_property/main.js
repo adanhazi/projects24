28d17da7ca76ce2d0ccc6c68796d0ff377715bf3
adanhazi <adan.hazi10@gmail.com>
2025-02-27 21:59:19 +0300

    unfinished str proprties

---
+++ JS_Functions/Str_property/main.js
@@ -0,0 +1,13 @@
+//length
+let greeting = "Hello, world!";
+console.log(greeting.length); // 13
+
+//Indexed Access as “Properties”
+let str = "JavaScript";
+console.log(str[0]); // "J"
+console.log(str[4]); // "S"
+
+//3. Inherited Properties from String’s Prototype
+//3.1. constructor
+let sample = "learn";
+console.log(sample.constructor === String); // true
