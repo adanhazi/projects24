6ffd0ca7ade94135a5eefb20400d3d73bf0bed8d
adanhazi <adan.hazi10@gmail.com>
2025-04-08 12:35:56 +0300

    handling independent promises function

---
+++ JS_Functions/handling_independent_promises/app.js
@@ -0,0 +1,9 @@
+import {cookBeans, steamBroccoli, cookRice, bakeChicken} from './library.js';
+async function serveDinner(){
+  const vegetablePromise = steamBroccoli();
+  const starchPromise = cookRice();
+  const proteinPromise = bakeChicken();
+  const sidePromise = cookBeans();
+  console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`);
+}
+serveDinner();
---
+++ JS_Functions/handling_independent_promises/library.js
@@ -0,0 +1,29 @@
+let cookBeans = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('beans');
+    }, 1000);
+  });
+}
+let steamBroccoli = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('broccoli')
+    }, 1000);
+  });
+}
+let cookRice = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('rice');
+    }, 100);
+  });
+}
+let bakeChicken = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('chicken');
+    }, 1000);
+  });
+}
+export default {cookBeans, steamBroccoli, cookRice, bakeChicken};
