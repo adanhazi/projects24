33f37ef5b7bc37bb478f9ec15440dd55ed928c6d
adanhazi <adan.hazi10@gmail.com>
2025-04-21 18:48:11 +0300

    substring string method second challenge

--- JS_Functions/String_Methods/substring/main.js
+++ JS_Functions/String_Methods/substring/main.js
@@ -7,3 +7,16 @@ function extractBetween(str, a, b) {
   if (a > b) [a, b] = [b, a];
   return str.substring(a, b);
 }
+
+/*
+Challenge 2: Remove First N
+Create removeFirstN(str, n) that removes the first n characters. If n ≤ 0, return the original string; if n ≥ str.length, return "".
+*/
+// Solution
+function removeFirstN(str, n) {
+  if (n <= 0) return str;
+  if (n >= str.length) return "";
+  return str.substring(n);
+}
+
+
