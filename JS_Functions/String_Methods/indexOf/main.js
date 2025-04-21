ed13122fc40b79e5b97c7e7e14e02fc79a336e28
adanhazi <adan.hazi10@gmail.com>
2025-04-21 11:05:21 +0300

    indexOf string method second challenge

--- JS_Functions/String_Methods/indexOf/main.js
+++ JS_Functions/String_Methods/indexOf/main.js
@@ -11,3 +11,19 @@ const positions = vowels
 .filter(pos => pos !== -1);
 return positions.length ? Math.min(...positions) : -1
 }
+
+/*
+Challenge 2: Last Occurrence vs .lastIndexOf()
+Without using .lastIndexOf(), write lastOccurrence(str, sub) that returns the last index of sub in str, or -1 if not found.
+Hint: Use .indexOf() in a loop, advancing the search start.
+*/
+//Solutions
+function lastOccurrence(str, sub) {
+  let lastPos = -1
+  let pos = str.indexOf(sub);
+  while (pos !== -1){
+      lastPos = pos
+      pos = str.indexOf(sub, pos + 1);
+}
+  return lastPos;
+}
