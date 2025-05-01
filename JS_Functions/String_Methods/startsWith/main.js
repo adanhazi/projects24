46dce36fbaf23b5f840c14cd5b7b29629e9c4045
adanhazi <adan.hazi10@gmail.com>
2025-05-01 12:22:53 +0300

    startsWith string method second challenge

--- JS_Functions/String_Methods/startsWith/main.js
+++ JS_Functions/String_Methods/startsWith/main.js
@@ -8,3 +8,14 @@ Return true if str begins with "Hello", otherwise false.
 function startsWithHello(str) {
   return str.startsWith('Hello')
 }
+
+/*
+Challenge 2: isValidPrefix(str, prefix, pos)
+Return true if str has prefix starting exactly at index pos.
+*/
+
+//Solutions
+
+function isValidPrefix(str, prefix, pos) {
+  return str.startsWith(prefix, pos);
+}
