0af43fae84877dacaf47aeb13b5d1d2d123fe51e
adanhazi <adan.hazi10@gmail.com>
2025-05-01 12:21:40 +0300

    startsWith string method first challenge

---
+++ JS_Functions/String_Methods/startsWith/main.js
@@ -0,0 +1,10 @@
+/*
+Challenge 1: startsWithHello(str)
+Return true if str begins with "Hello", otherwise false.
+*/
+
+//Solutions
+
+function startsWithHello(str) {
+  return str.startsWith('Hello')
+}
