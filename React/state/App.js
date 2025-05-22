4db36863a60ca071e46fae7a4c5b54ea31a5cbd5
adanhazi <adan.hazi10@gmail.com>
2025-05-22 21:52:27 +0300

    handleNextClick

--- React/state/App.js
+++ React/state/App.js
@@ -10,3 +10,7 @@ import { sculptureList } from './data.js';
 export default function Gallery() {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
